6e6df73cd8ea2ac6a78a414ce21c3e1d6a5b5ce8
arhoy <arhoy@ualberta.ca>
2019-08-23 12:41:37 -0600

    save token in cookies -- needed to tell application whether we are logged in our not

--- services/auth0.js
+++ services/auth0.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
 
 class Auth0 {
   constructor() {
@@ -9,11 +10,14 @@ class Auth0 {
       responseType: 'token id_token',
       scope: 'openid profile'
     });
+    // bind the methods below to the constructor
     this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
   }
 
   handleAuthentication() {
-    debugger;
     return new Promise((resolve, reject) => {
       this.auth0.parseHash((err, authResult) => {
         if (authResult && authResult.accessToken && authResult.idToken) {
@@ -26,11 +30,38 @@ class Auth0 {
       });
     });
   }
-  setSession() {
+  setSession(authResult) {
     //save tokens
+    let expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    // localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    // set Cookie to store user info for login and custom pages
+    Cookies.set('user', authResult.idTokenPayload);
+    Cookies.set('jwt', authResult.idTokenPayload);
+    Cookies.set('expiresAt', authResult.idTokenPayload);
   }
   login() {
     this.auth0.authorize();
+  }
+
+  logout() {
+    Cookies.remove('user');
+    Cookies.remove('jwt');
+    Cookies.remove('expiresAt');
+
+    // logging out
+    this.auth0.logout({
+      returnTo: '',
+      clientID: 'L3bX4QP8L8nea503SpcAZERBjebm27PM'
+    });
+  }
+
+  isAuthenticated() {
+    const expiresAt = Cookies.getJSON('expiresAt');
+    return new Date().getTime() < expiresAt;
   }
 }
 
